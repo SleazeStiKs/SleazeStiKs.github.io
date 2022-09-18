a3c026c56fd58a8ff9f432b4eab7927851bee364
Sleaze <sleazestiks@gmail.com>
2022-09-18 19:05:49 +0200

    Fixed Meta Parameters

--- src/site-meta.config.ts
+++ src/site-meta.config.ts
@@ -1,9 +1,9 @@
 const siteMeta = {
-  title: "Astro Theme Cactus",
-  description: "An opinionated starter theme for Astro",
-  lang: "en-GB",
-  ogLocale: "en_GB",
-  githubUrl: "https://github.com/chrismwilliams/astro-theme-cactus",
+  title: "Sleaze",
+  description: "Sleaze Personal Website",
+  lang: "en-US",
+  ogLocale: "en_US",
+  githubUrl: "https://github.com/sleazestiks/sleazestiks.github.io",
   themeColorLight: "#fafafa",
   themeColorDark: "#1d1f21",
 }
